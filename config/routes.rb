8c031053dcfce621db758080cdff5dfbc4524fcd
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-19 15:17:31 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/" ,{:controller=>"rps", :action=> "home"})
+  
+  get("/dice/:number_of_dice/:how_many_sides", {controller: "rps", action: "flexible"})
+
 end
